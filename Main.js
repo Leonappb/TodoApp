abf93ad5d9a06b7de8d8cdcf9a22bb3ff995c11d
Leona Ppaenburg <ljp.burg@gmail.com>
2025-01-24 10:24:33 +0100

    mein erster commit

---
+++ Main.js
@@ -0,0 +1,17 @@
+
+
+// for (let index = 0; index < 10; index++) {
+ 
+// alert(`Hello! I am an alert box ${index}!!`); 
+    
+// }
+
+//  while(true){
+//      console.log("Hallo");
+//  };
+//  
+/* <body bgcolor="#FF9933" text="#666666" link="#339900"vlink="#339900"
+ alink="#339900"><ahref="http://www.beepworld-hilfe.de"><scriptlanguage
+ ="JavaScript"><!--alert("Dein Text 1")alert("Dein Text 2")alert
+ ("Dein Text 3")alert("Dein Text 4")alert("Dein Text 5")// -->
+ </script></a><p align="center"><br><br><br></p> */
